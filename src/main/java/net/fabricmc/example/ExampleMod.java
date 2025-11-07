95c3396f1a4cd583610141e26855a64a30fc772e
Tyler <snowtyler15@gmail.com>
2025-11-06 21:42:11 -0500

    Convert to BTA (Better Than Adventure) mod - Replace Babric with BTA build system and dependencies

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -3,12 +3,23 @@ package net.fabricmc.example;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
 
-public class ExampleMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("click-mining-forever");
+public class ExampleMod implements ModInitializer, GameStartEntrypoint {
+	public static final String MOD_ID = "click-mining-forever";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Click Mining Forever initialized.");
+	}
+
+	@Override
+	public void beforeGameStart() {
 		LOGGER.info("Click Mining Forever loaded - mining delays removed!");
+	}
+
+	@Override
+	public void afterGameStart() {
 	}
 }
